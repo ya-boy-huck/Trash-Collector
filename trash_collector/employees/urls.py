874c85df95c0693dfd3c70b28f44af822de86f8f
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-10-25 17:00:38 -0400

    Created employee model based on the user story, and created the templates for the employee

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 app_name = "employees"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('edit_profile/', views.edit_profile, name="edit_profile"),
 ]
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from .models import Employee
 
 # Create your views here.
 
@@ -11,3 +14,24 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+
+def edit_profile(request):
+    logged_in_user = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        weekly_pickup_from_form = request.POST.get('weekly')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.address = address_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.weekly_pickup = weekly_pickup_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('customers:index'))
+    else:
+        context = {
+            'logged_in_employee': logged_in_employee
+        }
+        return render(request, 'employee/edit_profile.html', context)
