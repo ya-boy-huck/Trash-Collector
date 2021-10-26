e0f245d8e642b69108416f18dbf8ec9afc453469
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-10-26 12:06:59 -0400

    Completed Employee registration and fixed the nav bar for employees

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -8,25 +8,23 @@ from datetime import date
 from .models import Employee
 
 
-# Create your views here.
-
-# TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
-
 @login_required
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
-    try:
-        all_customers = Customer.objects.all()
 
+    Customer = apps.get_model('customers.Customer')
+    logged_in_user = request.user
+    try:
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+        
         today = date.today()
     
         context = {
-            'all_customers': all_customers,
+            'logged_in_employee': logged_in_employee,
             'today': today
         }
 
-        return render(request, 'employees/index.html')
+        return render(request, 'employees/index.html', context)
     except ObjectDoesNotExist:
         return HttpResponseRedirect(reverse('employees:create'))
 
@@ -51,7 +49,6 @@ def edit_profile(request):
         name_from_form = request.POST.get('name')
         address_from_form = request.POST.get('address')
         zip_from_form = request.POST.get('zip_code')
-        weekly_pickup_from_form = request.POST.get('weekly')
         logged_in_employee.name = name_from_form
         logged_in_employee.address = address_from_form
         logged_in_employee.zip_code = zip_from_form
